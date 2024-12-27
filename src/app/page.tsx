58999316e350ec4c8beb74bb76fe278b5b17f0ec
Douglas <douglaswelcome@gmail.com>
2024-12-26 18:40:03 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-1 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-dm-sans)]">
+      <main className="flex flex-col justify-items-center items-center gap-4">
         <Image
           className=""
           src="/dwlogo.svg"
@@ -12,11 +12,11 @@ export default function Home() {
           height={38}
           priority
         />
-        <div className="text-sm text-center font-[family-name:var(--font-geist-mono)]">
-            Portfolio for Douglas Welcome coming soon...
+        <div className="text-sm text-center font-[family-name:var(--font-dm-sans)]">
+            <p>Portfolio for Douglas Welcome coming soon.</p>
         </div>
-        <div className="text-sm text-center font-[family-name:var(--font-geist-mono)]">
-            Please email <a href="mailto:hello@douglaswelcome.com">hello@douglaswelcome.com</a> to get in touch and view work samples.
+        <div className="text-sm text-center font-[family-name:var(--font-dm-sans)]">
+            <p>Please email <a href="mailto:hello@douglaswelcome.com">hello@douglaswelcome.com</a> to get in touch and view work samples.</p>
         </div>
       </main>
     </div>
